e475d28f611adee94f393d8951fda497a51d8926
Adam_Gadaczek <adam.gadaczek@wp.pl>
2025-09-28 08:58:54 +0200

    Add authors route

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,18 @@
 from app import app
 from flask import render_template
-from app.models import Book
+from app.models import Book, Author
 import requests
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/books")
+@app.route("/books", strict_slashes=False)
 def books():
     all_books = Book.query.all()
     return render_template("books.html", books=all_books)
+
+@app.route("/authors", strict_slashes=False)
+def authors():
+    all_authors = Author.query.all()
+    return render_template("authors.html", authors=all_authors)
